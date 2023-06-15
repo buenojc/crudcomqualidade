f74ef82c9b8fd1d007cca006c64ebf1efc4196ed
Julio Cesar Bueno <buenojc@outlook.com>
2023-06-15 18:47:33 -0300

    add: estrutura inicial de criação

---
+++ core/crud.js
@@ -0,0 +1,15 @@
+const fs = require('fs')
+const DB_FILE_PATH = './core/db'
+
+console.log('[CRUD]')
+
+function create(content){
+    // salvar o content no sistema
+    fs.writeFileSync(DB_FILE_PATH, content)
+    return content
+}
+
+
+// [SIMULATION]
+
+console.log(create('Este é um novo teste'))
