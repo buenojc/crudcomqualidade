71cc9091d772aa7f69bc7b62fe143aab6c8d0adf
Julio Cesar Bueno <buenojc@outlook.com>
2023-07-06 11:29:03 -0300

    add: função delete e criação de type

--- core/crud.ts
+++ core/crud.ts
@@ -4,9 +4,13 @@ import { v4 as uuid } from "uuid";
 
 console.log("[CRUD]");
 
+// Esse tipo serve para dar mais clareza às funções. O UUID é uma string, mas criando um tipo para ela,
+// em todas as funções e tipos podemos adicionar o TIPO UUID, sendo mais claro qual o dado para quem está lendo a função. 
+type UUID = string;
+
 // interface é um tipo de molde para aquele dado. No caso, um TODO deve conter todos os dados abaixo e com os respectivos tipos
 interface Todo {
-  id: string;
+  id: UUID;
   date: string;
   content: string;
   done: boolean;
@@ -43,7 +47,7 @@ function read(): Array<Todo> {
 }
 
 // Esse partial indica que pode receber alguma coisa do Todo, ou seja todos os atributos podem existir ou serem nulos
-function update(id: string, partialTodo: Partial<Todo>): Todo {
+function update(id: UUID, partialTodo: Partial<Todo>): Todo {
   let updatedTodo;
   const todos = read();
   todos.forEach((currentTodo) => {
@@ -65,10 +69,24 @@ function update(id: string, partialTodo: Partial<Todo>): Todo {
 
 // Normalmente em um sistema em produção não é possível alterar um objeto completamente,
 // Então é mais comum usar funções que atualizam uma coisa só, como essa de atualizar só content
-function updateContentById(id: string, content:string): Todo{
+function updateContentById(id: UUID, content:string): Todo{
     return update(id, {
         content
     })
+}
+
+function deleteById(id: UUID){
+  const todos = read()
+  const todosWithoutOne = todos.filter((todo) => {
+    // No .filter o o que for verdadeiro fica no filtro, o que for falso sai. 
+    // É retornado um array com todos os itens que retornaram true
+    if(id === todo.id){
+      return false
+    }
+    return true
+  })
+
+  fs.writeFileSync(DB_FILE_PATH, JSON.stringify({ todos: todosWithoutOne }, null, 2));
 }
 
 
@@ -79,7 +97,8 @@ function CLEAR_DB() {
 // [SIMULATION]
 CLEAR_DB();
 create("Primeira TODO");
-create("Segunda TODO");
-const terceiraTodo = create("Terceira TODO");
-updateContentById(terceiraTodo.id, 'Ultima Atualização TODO')
-console.log(read());
+const secondTodo = create("Segunda TODO");
+const thirdTodo = create("Terceira TODO");
+updateContentById(thirdTodo.id, 'Ultima Atualização TODO')
+deleteById(secondTodo.id)
+console.log(read())
